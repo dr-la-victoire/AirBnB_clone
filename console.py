87110a4eb79a734128b624cb1d6a7199e406d8c9
La Victoire <doctorvictory116@gmail.com>
2023-08-13 17:01:45 +0100

    Task 6

---
+++ console.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""This module is the console for Airbnb"""
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """This class creates the console"""
+    prompt = "(hbnb) "
+
+    def do_quit(self, line):
+        """Quit to exit the program"""
+        return True
+
+    def do_EOF(self, line):
+        """Exits the program"""
+        return True
+
+    def emptyline(self):
+        """Does nothing"""
+        pass
+
+
+if __name__ == "__main__":
+    HBNBCommand().cmdloop()
