ec96f314d085c75b225e03bfe5eccdf80c432f14
La Victoire <doctorvictory116@gmail.com>
2023-08-13 16:41:47 +0100

    will update later

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""This module contains test cases"""
+import unittest
+from models.base_model import BaseModel
+
+
+class TestBaseModel(unittest.TestCase):
+    """This class contains the test cases for BaseModel"""
+    pass
+
+
+class TestBaseModelSave(unittest.TestCase):
+    """This class tests the save()"""
+    pass
+
+
+class TestBaseModelToDict(unittest.TestCase):
+    """This class tests the to_dict()"""
+    pass
+
+
+if __name__ == "__main__":
+    unittest.main()
