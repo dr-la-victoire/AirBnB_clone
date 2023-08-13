54d54086c14959e3c24d380730b98044421465f1
La Victoire <doctorvictory116@gmail.com>
2023-08-13 16:27:59 +0100

    Task 3

---
+++ models/base_model.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""This module is the base model for the Airbnb project"""
+import uuid
+from datetime import datetime
+
+
+class BaseModel():
+    """This class contains the common attributes and methods"""
+
+    def __init__(self):
+        """Initializes the class"""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        """Returns a str representation of an instance"""
+        return "[{}] ({}) {}".format(
+            self.__class__.__name__, self.id, self.__dict__)
+
+    def save(self):
+        """Updates the attribute 'updated_at'"""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """Returns a dictionary of the instance"""
+        the_dict = self.__dict__
+        the_dict["__class__"] = self.__class__.__name__
+        the_dict["created_at"] = the_dict["created_at"].isoformat()
+        the_dict["updated_at"] = the_dict["updated_at"].isoformat()
+        return the_dict
