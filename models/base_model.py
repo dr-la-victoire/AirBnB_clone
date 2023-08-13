777aed12727090bbae7cda2b6458031f6f0e6ed9
La Victoire <doctorvictory116@gmail.com>
2023-08-13 16:51:50 +0100

    Task 4

--- models/base_model.py
+++ models/base_model.py
@@ -7,11 +7,24 @@ from datetime import datetime
 class BaseModel():
     """This class contains the common attributes and methods"""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initializes the class"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs is not None and kwargs != {}:
+            for key in kwargs:
+                if key == "__class__":
+                    continue
+                elif key == "created_at":
+                    self.__dict__["created_at"] = datetime.strptime(
+                        kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                elif key == "updated_at":
+                    self.__dict__["updated_at"] = datetime.strptime(
+                        kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                else:
+                    self.__dict__[key] = kwargs[key]
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """Returns a str representation of an instance"""
