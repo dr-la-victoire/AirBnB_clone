92c69fb04cc71fc16207253eee2bc5fe4ee105a0
La Victoire <doctorvictory116@gmail.com>
2023-08-13 20:52:26 +0100

    Task 9 - Incomplete

---
+++ models/city.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This module has the City class"""
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """Inherits from BaseModel"""
+    name = ""
+    state_id = ""
---
+++ models/state.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This module has the State class"""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """Inherits from BaseModel"""
+    name = ""
