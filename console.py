b9d17e2990cea2c62160e05ee45b128d16e54f91
La Victoire <doctorvictory116@gmail.com>
2023-08-13 17:06:28 +0100

    Task 6

--- console.py
+++ console.py
@@ -8,11 +8,11 @@ class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb) "
 
     def do_quit(self, line):
-        """Quit to exit the program"""
+        """Quit command to exit the program"""
         return True
 
     def do_EOF(self, line):
-        """Exits the program"""
+        """EOF signal to exit the program"""
         print("")
         return True
 
